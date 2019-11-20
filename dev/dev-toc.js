5bec42cb3e297496bde0b96111d5e792d76a807a
wbluke <pwb0127@naver.com>
2019-11-21 01:42:45 +0900

    feat: 최상위 태그를 판별하고 그에 따른 레벨을 부여하는 로직 추가

--- dev/dev-toc.js
+++ dev/dev-toc.js
@@ -9,22 +9,45 @@ const TOC = {
             </el-card>`
 }
 
-const LEVEL_1 = 1;
-const LEVEL_2 = 2;
-const LEVEL_3 = 3;
-
 const CONSTANTS = (function() {
+  const NUM_OF_H1 = 1;
+  const NUM_OF_H2 = 2;
+  const NUM_OF_H3 = 3;
 
+  const LEVEL_1 = 1;
+  const LEVEL_2 = 2;
+  const LEVEL_3 = 3;
+
+  /* Level 별 들여쓰기 공백 개수 */
   const BLANKS_OF_LEVEL_1 = 0;
   const BLANKS_OF_LEVEL_2 = 4;
   const BLANKS_OF_LEVEL_3 = 8;
 
   const blanksByLevel = function() {
-    return new Map([[LEVEL_1, BLANKS_OF_LEVEL_1], [LEVEL_2, BLANKS_OF_LEVEL_2], [LEVEL_3, BLANKS_OF_LEVEL_3]]);
+    return new Map([[NUM_OF_H1, BLANKS_OF_LEVEL_1], [NUM_OF_H2, BLANKS_OF_LEVEL_2], [NUM_OF_H3, BLANKS_OF_LEVEL_3]]);
+  }
+
+  /* 최상위 태그에 따른 레벨 Map */
+  const levelsByH1 = function() {
+    return new Map([[NUM_OF_H1, LEVEL_1], [NUM_OF_H2, LEVEL_2], [NUM_OF_H3, LEVEL_3]])
+  }
+
+  const levelsByH2 = function() {
+    return new Map([[NUM_OF_H2, LEVEL_1], [NUM_OF_H3, LEVEL_2]])
+  }
+
+  const levelsByH3 = function() {
+    return new Map([[NUM_OF_H3, LEVEL_1]])
   }
 
   return {
-    blanksByLevel: blanksByLevel()
+    numOfH1: NUM_OF_H1,
+    numOfH2: NUM_OF_H2,
+    numOfH3: NUM_OF_H3,
+    blanksByLevel: blanksByLevel(),
+    levelsByH1: levelsByH1(),
+    levelsByH2: levelsByH2(),
+    levelsByH3: levelsByH3(),
   }
 })();
 
@@ -40,28 +63,66 @@ new Vue({
     const mainContents = document.querySelector('.area_view');
     const tocTags = mainContents.querySelectorAll('h1, h2, h3');
 
-    // TODO : 최상위 태그 판별 작업
+    /* h1, h2, h3 태그가 없는 경우 */
+    if (tocTags.length == 0) {
+      return;
+    }
 
-    // TOC에 태그 삽입
+    /** 최상위 태그에 따른 레벨 Map 받아오기
+     * 
+     * h1 ~ h3 태그 중 가장 높은 태그를 찾아서 그에 맞게 Level을 설정한다.
+     * 예를 들어, h1 태그가 없고 h2, h3 태그만 있는 경우
+     * h2가 가장 높은 태그이며, 해당 태그 h2에 LEVEL_1을 부여하고 그 다음 태그인 h3에는 LEVEL_2를 부여한다.
+     * 
+     * 부여된 Level에 따라 적용되는 CSS가 달라진다.
+     *  */ 
+    const getLevelsByHighestTag = function() {
+
+      /* 최상위 태그 판별 작업 */
+      const findHighestHTagName = function() {
+        const ret = Array.prototype.slice.call(tocTags).reduce((pre, cur) => {
+          const numOfPre = Number(pre.tagName.slice(-1));
+          const numOfCur = Number(cur.tagName.slice(-1));
+
+          return (numOfPre < numOfCur) ? pre : cur;
+        })
+
+        return ret.tagName
+      }
+
+      const highestHTagName = findHighestHTagName();
+
+      if ('H1'.match(highestHTagName)) {
+        return CONSTANTS.levelsByH1;
+      }
+
+      if ('H2'.match(highestHTagName)) {
+        return CONSTANTS.levelsByH2;
+      }
+
+      return CONSTANTS.levelsByH3;
+    }
+
+    const levelMap = getLevelsByHighestTag();
+
+    /* TOC에 태그 삽입 */ 
     const elCard = document.querySelector('#toc-elements');
     tocTags.forEach(element => {
       const text = element.innerText;
+
       let item;
-      if (element.matches('h1')) {
-        item = this.getTagTemplateByLevel(LEVEL_1, text);
-      }
-      if (element.matches('h2')) {
-        item = this.getTagTemplateByLevel(LEVEL_2, text);
-      }
-      if (element.matches('h3')) {
-        item = this.getTagTemplateByLevel(LEVEL_3, text);
-      }
+
+      levelMap.forEach((value, key) => {
+        if (element.matches(`h${key}`)) {
+          item = this.getTagTemplateByLevel(value, text);
+        }
+      })
 
       elCard.appendChild(item);
     });
   },
   methods: {
-    getTagTemplateByLevel: function(level = LEVEL_1, text = '') {
+    getTagTemplateByLevel: function(level = CONSTANTS.NUM_OF_H1, text = '') {
       const basicItem = this.getBasicItemTemplate(text);
       const blanks = this.generateBlanks(CONSTANTS.blanksByLevel.get(level)); 
 
